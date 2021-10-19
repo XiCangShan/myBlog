19c8c168f131bdb90cf417c87cc2399868336876
xcs <xcs@qq.com>
2021-10-19 20:33:16 +0800

    master-1

--- src/main/java/com/myall/myBlog/pojo/Gittest.java
+++ src/main/java/com/myall/myBlog/pojo/Gittest.java
@@ -2,6 +2,6 @@ package com.myall.myBlog.pojo;
 
 public class Gittest {
     public static void main(String[] args) {
-        System.out.println("test v1.01");
+        System.out.println("test v1.01  master");
     }
 }
