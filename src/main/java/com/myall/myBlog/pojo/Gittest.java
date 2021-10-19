5b932874d7ce2726ab4713545c5ee78a87ff11ac
xcs <xcs@qq.com>
2021-10-19 20:25:41 +0800

    hot-fix

--- src/main/java/com/myall/myBlog/pojo/Gittest.java
+++ src/main/java/com/myall/myBlog/pojo/Gittest.java
@@ -3,5 +3,6 @@ package com.myall.myBlog.pojo;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("test v1.01");
+        System.out.println("test v1.02 hot-fix");
     }
 }
